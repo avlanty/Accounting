3cc3f8b6d8bf037049e7dc8a1a6957b77748ed22
Anthony Alvarado <anthonydalvarado@gmail.com>
2023-05-01 10:08:16 -0400

    work between stations, also added options

--- src/main/java/org/yup/accountant/AccountingLedgerApp.java
+++ src/main/java/org/yup/accountant/AccountingLedgerApp.java
@@ -10,25 +10,23 @@ public class AccountingLedgerApp {
     public static void main(String[] args) {
 
         try {
-            //calling in stuff for us to use
+            // calling in stuff for us to use
             FileWriter usingFile = new FileWriter("transactions.csv");
             BufferedWriter editFile = new BufferedWriter(usingFile);
             Scanner scanner = new Scanner(System.in);
 
             System.out.println("Welcome to the bank!");
             System.out.print(UserInput.dOrW());
-            String testAns = scanner.nextLine();
-            System.out.println();
             int numOfFields = 5;
 
-            //getting info to send into the file
+            // getting info to send into the file
             System.out.println("Enter the required info for your transactions. \n");
             for (int i = 0; i < numOfFields; i++){
                 System.out.print(UserInput.getInfoQuest(i));
                 String input = scanner.nextLine();
                 editFile.write(input);
 
-                //only adds the pipe if it is the last field
+                // only adds the pipe if it is the last field
                 if(i != numOfFields - 1){
                     editFile.write("|");
                 }
--- src/main/java/org/yup/accountant/UserInput.java
+++ src/main/java/org/yup/accountant/UserInput.java
@@ -1,17 +1,34 @@
 package org.yup.accountant;
+import java.util.Objects;
+import java.util.Scanner;
 
 public class UserInput {
 
-    //method to return infoQuest for transactions
+    // method to return infoQuest for transactions
     public static String getInfoQuest(int field){
         String[] infoQuest = {"Enter date (YYYY-MM-DD): ", "Enter time (HH:MM:SS): ", "Enter description: ", "Enter vendor: ", "Enter amount: $"};
         return infoQuest[field];
     }
 
-    /*method to allow user to choose if their making
+    /* method to allow user to choose if their making
     a deposit or a withdrawal*/
     public static String dOrW(){
-        return "Will you be making a Deposit or Withdrawal? (D/W): ";
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Will you be making a Deposit or Withdrawal? (D/W): ");
+        String choice = scanner.next();
+        System.out.print("\n");
+
+        /* if you choose W or D, it will tell you what you chose
+        I also want it to give you an error if you choose incorrectly */
+        if(Objects.equals(choice.toLowerCase(), "d")){
+            return "You chose to make a deposit.\n";
+        }else if(Objects.equals(choice.toLowerCase(), "w")){
+            return "You chose to make a withdrawal.\n";
+        }else{
+            return "Error: Wrong Input.\n";
+        }
+
     }
 
 }
